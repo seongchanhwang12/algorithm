a79d782e7890a86d053e8eaa2f976d552e3c1c0d
seongchanhwang <tjdcksghkd12@gmail.com>
2024-12-07 20:57:26 +0900

    11021 수정

--- src/backjoon/backjoon_11021.java
+++ src/backjoon/backjoon_11021.java
@@ -1,3 +1,4 @@
+package backjoon;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -5,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 
-public class Main {
+public class backjoon_11021 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
