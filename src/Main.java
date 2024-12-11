9669b511217d0d2f8f4863c8e0f52eefe7b24a8f
seongchanhwang <tjdcksghkd12@gmail.com>
2024-12-11 23:57:01 +0900

    back joon 10810 공 넣기 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,10 @@
 import java.io.*;
-import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> list = new ArrayList<>();
-        int max = 0;
-        for (int i = 0; i < 9; i++) {
-            int n = Integer.parseInt(br.readLine());
-            list.add(n);
-            max = Math.max(max,n);
-        }
-
-        System.out.println(max);
-        System.out.println(list.indexOf(max)+1);
-
-    }
-
-
-    public static void main2(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int[] numbers = new int[9];
-
-        int maxIdx = -1;
-        int max = Integer.MIN_VALUE;
-
-        for (int i = 0; i < 9; i++) {
-            numbers[i] = Integer.parseInt( br.readLine());
-
-            if(numbers[i]>max){
-                maxIdx = i;
-                max = numbers[i];
-            }
-        }
-        sb.append(max).append("\n").append(maxIdx+1);
-
-        System.out.println(sb);
+        BufferedReader br = new BufferedReader(new InputStreamReader( System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
     }
 
