89958942b7a309ee65be09c7172372fd6775f2ad
seongchanhwang <tjdcksghkd12@gmail.com>
2024-12-19 19:57:00 +0900

    1546 - 평균

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,32 @@ import java.io.*;
 import java.util.*;
 
 /**
- * 문제
- * 도현이는 바구니를 총 N개 가지고 있고, 각각의 바구니에는 1번부터 N번까지 번호가 매겨져 있다.
- * 바구니에는 공이 1개씩 들어있고, 처음에는 바구니에 적혀있는 번호와 같은 번호가 적힌 공이 들어있다.
  *
- * 도현이는 앞으로 M번 공을 바꾸려고 한다.
- * 도현이는 공을 바꿀 바구니 2개를 선택하고, 두 바구니에 들어있는 공을 서로 교환한다.
- *
- * 공을 어떻게 바꿀지가 주어졌을 때, M번 공을 바꾼 이후에 각 바구니에 어떤 공이 들어있는지 구하는 프로그램을 작성하시오.
- *
- * 입력
- * 첫째 줄에 N (1 ≤ N ≤ 100)과 M (1 ≤ M ≤ 100)이 주어진다.
- *
- * 둘째 줄부터 M개의 줄에 걸쳐서 공을 교환할 방법이 주어진다. 각 방법은 두 정수 i j로 이루어져 있으며, i번 바구니와 j번 바구니에 들어있는 공을 교환한다는 뜻이다. (1 ≤ i ≤ j ≤ N)
- *
- * 도현이는 입력으로 주어진 순서대로 공을 교환한다.
- *
- * 출력
- * 1번 바구니부터 N번 바구니에 들어있는 공의 번호를 공백으로 구분해 출력한다.
  */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader( System.in));
+        // 시험본 과목개수 N
+        int N = Integer.parseInt(br.readLine());
+        int[] scores = new int[N];
+
+        // 현재 점수
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int max = -1 ;
+
+        for (int i = 0; i < N; i++) {
+            scores[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, scores[i]);
+        }
+
+        double avg = 0;
+        for (int score : scores) {
+            avg += (double) score / max * 100;
+
+        }
+
+        System.out.print(avg / scores.length + " ");
 
     }
 
