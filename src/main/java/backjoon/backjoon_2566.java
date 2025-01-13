97a33fa50f9cc44e4a803fae81350ed3300c08c7
seongchanhwang12 <tjdcksghkd12@gmail.com>
2025-01-13 23:06:14 +0900

    [백준] 2566 - 최대값

--- src/main/java/backjoon/backjoon_2566.java
+++ src/main/java/backjoon/backjoon_2566.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class backjoon_2566 {
 
     public static void main(String[] args) throws IOException {
         // 최댓값, 몇행 몇 열
